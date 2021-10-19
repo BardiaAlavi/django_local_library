62a1f56a01f119acf0415af0cf7eb8b5b06edc2e
BardiaAlavi <86231831+BardiaAlavi@users.noreply.github.com>
2021-10-19 17:18:45 +1100

    Update settings.py

--- ArezooNameh/settings.py
+++ ArezooNameh/settings.py
@@ -136,3 +136,8 @@ INTERNAL_IPS=['127.0.0.1']
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
